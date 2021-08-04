b1c7f50fca5cfe4db787eb979511eb816ed9a35b
Apel Mahmud <apelmahmud.dev@gmail.com>
2021-08-04 08:24:31 +0600

    Closed issue is solved

--- main.js
+++ main.js
@@ -25,7 +25,7 @@ function submitIssue(e) {
 
 const closeIssue = (id) => {
 	const issues = JSON.parse(localStorage.getItem('issues'));
-	const currentIssue = issues.find((issue) => issue.id === id);
+	const currentIssue = issues.find((issue) => issue.id == id);
 	currentIssue.status = 'Closed';
 	localStorage.setItem('issues', JSON.stringify(issues));
 	fetchIssues();
@@ -38,6 +38,7 @@ const deleteIssue = (id) => {
 	fetchIssues();
 };
 
+// fetch issues when update dom
 const fetchIssues = () => {
 	const issues = JSON.parse(localStorage.getItem('issues'));
 	// display the issues quantity
@@ -54,7 +55,7 @@ const fetchIssues = () => {
                               <h3> ${description} </h3>
                               <p><span class="glyphicon glyphicon-time"></span> ${severity}</p>
                               <p><span class="glyphicon glyphicon-user"></span> ${assignedTo}</p>
-                              <a href="#" onclick="setStatusClosed(${id})" class="btn btn-warning">Close</a>
+                              <a href="#" onclick="closeIssue(${id})" class="btn btn-warning">Close</a>
                               <a href="#" onclick="deleteIssue(${id})" class="btn btn-danger">Delete</a>
                               </div>`;
 	}
