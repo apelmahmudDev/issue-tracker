7b9efbd812edbd2f1213b74f6b8782823064f444
Apel Mahmud <apelmahmud.dev@gmail.com>
2021-08-04 09:09:10 +0600

    All bugs is solved and now it is working fine

--- main.js
+++ main.js
@@ -59,7 +59,7 @@ const fetchIssues = () => {
 		issuesList.innerHTML += `<div class="well">
                               <h6>Issue ID: ${id} </h6>
                               <p><span class="label label-info"> ${status} </span></p>
-                              <h3> ${description} </h3>
+                              <h3 id="line-through"> ${description} </h3>
                               <p><span class="glyphicon glyphicon-time"></span> ${severity}</p>
                               <p><span class="glyphicon glyphicon-user"></span> ${assignedTo}</p>
                               <a href="#" onclick="closeIssue(${id})" class="btn btn-warning">Close</a>
